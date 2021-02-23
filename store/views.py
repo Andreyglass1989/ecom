a37a08404f80cff989c5636186211bad8acecbb2
Andreyglass1989 <1989andreyglass@gmail.com>
2021-02-23 10:04:10 +0200

    after add cart functional

--- store/views.py
+++ store/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from .models import *
 
+import json
+from django.http import JsonResponse
 # Create your views here.
 
 def store(request):
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
+	else:
+		items = []
+		order = {"get_cart_total":0, 
+				 "get_cart_items":0}
+		cartItems = order['get_cart_items']
+
 	products = Product.objects.all()
-	context = {'products':products}
+	context = {'products':products, 'cartItems':cartItems}
 	return render(request, 'store/Store.html', context)
 
 
@@ -14,12 +27,14 @@ def cart(request):
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
 	else:
 		items = []
 		order = {"get_cart_total":0, 
 				 "get_cart_items":0}
+		cartItems = order['get_cart_items']
 
-	context = {'items':items, 'order':order}
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/Cart.html', context)
 
 
@@ -28,10 +43,35 @@ def checkout(request):
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
 	else:
 		items = []
 		order = {"get_cart_total":0, 
 				 "get_cart_items":0}
+		cartItems = order['get_cart_items']
 
-	context = {'items':items, 'order':order}
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/Checkout.html', context)
+
+
+def updateItem(request):
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print("Action: ", action)
+	print("ProductId: ", productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+	if orderItem.quantity <=0:
+		orderItem.delete()
+	return JsonResponse('Item was added', safe=False)
